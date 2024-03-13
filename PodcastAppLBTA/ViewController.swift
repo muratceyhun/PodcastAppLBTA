22b3a44b723a748c6382e64264a8a5df87646b29
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2024-03-13 16:35:04 +0300

    Creating the project

---
+++ PodcastAppLBTA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastAppLBTA
+//
+//  Created by Murat Ceyhun Korpeoglu on 13.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
