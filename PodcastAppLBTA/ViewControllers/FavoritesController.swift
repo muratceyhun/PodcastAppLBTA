45222cff73f3bb383125194510c85e9b10724859
Murat Ceyhun KORPEOGLU <muratceyhunkorpeoglu@gmail.com>
2024-04-02 18:27:39 +0300

    Creating the form of FavoritesControllerView

--- PodcastAppLBTA/ViewControllers/FavoritesController.swift
+++ PodcastAppLBTA/ViewControllers/FavoritesController.swift
@@ -9,10 +9,50 @@ import UIKit
 
 class FavoritesController: BaseListController {
     
+    let cellID = "cellID"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        setupCollectionView()
     }
+    
+    fileprivate func setupCollectionView() {
+        collectionView.backgroundColor = .purple
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
+        cell.backgroundColor = .orange
+        return cell
+    }
+}
+
+
+//MARK: - UICollectionViewFlowLayout
+
+
+extension FavoritesController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = (collectionView.frame.width - 3 * 16) / 2
+        return .init(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 16, left: 16, bottom: 16, right: 16)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+    
 }
